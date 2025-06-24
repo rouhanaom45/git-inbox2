7545595bcd3ea006e197a857ae6751c8bf6f82d9
rouhanaom45 <200379123+rouhanaom45@users.noreply.github.com>
2025-06-25 01:01:25 +0200

    Create start1.sh

---
+++ start1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+scripts=(
+  "get-profile.py"
+  "profile-txt.py"
+  "open-profile.sh"
+  "proton1.py"
+  "confirm0.py"
+  "proton2.py"
+  "upload1.py"
+  "save.py"
+  "github1.py"
+  "github2.py"
+  "confirm.py"
+  "repository.py"
+  "upload.py"
+)
+
+# Run each script in order
+for script in "${scripts[@]}"; do
+  echo "Running $script..."
+  python3 "$script"
+  if [ $? -ne 0 ]; then
+    echo "Error: $script failed. Exiting."
+    exit 1
+  fi
+  echo "$script finished successfully."
+done
+
+sleep 2
